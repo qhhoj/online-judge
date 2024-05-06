14b7970551bf2f7b3a399c1e70351917594d9c46
Phan Bình Nguyên Lâm <76941117+LLaammTTeerr@users.noreply.github.com>
2024-05-06 21:46:02 +0700

    feat: create printable password sheet in batchadduser command

--- judge/management/commands/batchadduser.py
+++ judge/management/commands/batchadduser.py
@@ -10,17 +10,65 @@ from judge.models import Language, Profile
 
 ALPHABET = string.ascii_letters + string.digits
 
+html_header = '''
+<style>
+    .card {
+        font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+        font-size: 28px;
+        width: 800px;
+        /*background-color: rgb(230, 230, 230);*/
+        border: 3px solid #D20062;
+        border-radius: 5px;
+        padding: 20px;
+        margin: 20px;
+        break-inside: avoid;
+    }
+
+    .participantName {
+        font-size: 30px;
+        font-weight: bold;
+    }
+
+    code {
+        color: #D20062;
+        padding: 0 20px 0 0;
+    }
+
+    p {
+        margin: 10px 10px 10px 0;
+    }
+</style>
+'''
+
+user_html_template = '''
+<div class="card ">
+    <div class="participantName">{fullname}</div>
+
+    <p>
+        login:
+        <code>{username}</code>
+        password:
+        <code>{password}</code>
+    </p>
+
+    <p>
+        website:
+        <code>{site_url}</code>
+    </p>
+</div>
+'''
+
 
 def generate_password():
     return ''.join(secrets.choice(ALPHABET) for _ in range(8))
 
 
 def add_user(username, fullname, password):
-    usr = User(username=username, first_name=fullname, is_active=True, is_unlisted=True)
+    usr = User(username=username, first_name=fullname, is_active=True)
     usr.set_password(password)
     usr.save()
 
-    profile = Profile(user=usr)
+    profile = Profile(user=usr, is_unlisted=True)
     profile.language = Language.objects.get(key=settings.DEFAULT_USER_LANGUAGE)
     profile.save()
 
@@ -31,6 +79,7 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('input', help='csv file containing username and fullname')
         parser.add_argument('output', help='where to store output csv file')
+        parser.add_argument('--html', dest='html', help='where to store print password html file')
 
     def handle(self, *args, **options):
         fin = open(options['input'], 'r', encoding='utf-8-sig')
@@ -40,6 +89,8 @@ class Command(BaseCommand):
         writer = csv.DictWriter(fout, fieldnames=['username', 'fullname', 'password'])
         writer.writeheader()
 
+        usr_list = []
+
         for row in reader:
             username = row['username']
             fullname = row['fullname']
@@ -47,11 +98,19 @@ class Command(BaseCommand):
 
             add_user(username, fullname, password)
 
+            usr_list.append((username, fullname, password))
+
             writer.writerow({
                 'username': username,
                 'fullname': fullname,
                 'password': password,
             })
 
+        if options['html'] is not None:
+            fhtml = open(options['html'], 'w')
+            fhtml.write(html_header)
+
+            for username, fullname, password in usr_list:
+                fhtml.write(user_html_template.format(username=username, fullname=fullname, password=password, site_url=settings.SITE_FULL_URL))
         fin.close()
         fout.close()
