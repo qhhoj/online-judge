88ec583c3ac1bf5e40a4e9634659daa08ce0bfb0
Phan Bình Nguyên Lâm <76941117+LLaammTTeerr@users.noreply.github.com>
2024-05-06 20:29:01 +0700

    feat: fix csv-reader for official contest ranking

--- judge/utils/cms.py
+++ judge/utils/cms.py
@@ -6,15 +6,15 @@ def parse_csv_ranking(raw):
     reader = csv.DictReader(io.StringIO(raw))
 
     show_team = 'Team' in reader.fieldnames
-    problems = [reader.fieldnames[i] for i in range(3 if show_team else 2, len(reader.fieldnames) - 2, 2)]
+    problems = [reader.fieldnames[i] for i in range(5 if show_team else 4, len(reader.fieldnames), 1)]
     users = []
 
     for row in reader:
         users.append({
             'username': row['Username'],
-            'full_name': row['User'],
-            'scores': [float(row[prob]) for prob in problems],
-            'total_score': float(row['Global']),
+            'full_name': row['Full Name'],
+            'scores': [float(row[prob]) if row[prob] != '' else 0 for prob in problems],
+            'total_score': float(row['Points']),
         })
 
     users.sort(key=lambda p: p['total_score'], reverse=True)
