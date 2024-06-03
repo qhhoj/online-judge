5716cd90cb1e6fa8c83c33af17053d3fa8ab2b2c
Phan Bình Nguyên Lâm <76941117+LLaammTTeerr@users.noreply.github.com>
2024-06-03 08:34:20 +0700

    refactor: clean code for IOI contest format score computing

--- judge/contest_format/ioi.py
+++ judge/contest_format/ioi.py
@@ -1,9 +1,8 @@
-from django.db import connection
 from django.utils.translation import gettext as _, gettext_lazy
 
 from judge.contest_format.legacy_ioi import LegacyIOIContestFormat
 from judge.contest_format.registry import register_contest_format
-from judge.timezone import from_database_time
+# from judge.timezone import from_database_time
 
 
 @register_contest_format('ioi16')
@@ -19,78 +18,21 @@ class IOIContestFormat(LegacyIOIContestFormat):
         score = 0
         format_data = {}
 
-        with connection.cursor() as cursor:
-            cursor.execute("""
-                SELECT q.prob,
-                       MIN(q.date) as `date`,
-                       q.batch_points
-                FROM (
-                         SELECT cp.id          as `prob`,
-                                sub.id         as `subid`,
-                                sub.date       as `date`,
-                                tc.points      as `points`,
-                                tc.batch       as `batch`,
-                                MIN(tc.points) as `batch_points`
-                         FROM judge_contestproblem cp
-                                  INNER JOIN
-                              judge_contestsubmission cs
-                              ON (cs.problem_id = cp.id AND cs.participation_id = %s)
-                                  LEFT OUTER JOIN
-                              judge_submission sub
-                              ON (sub.id = cs.submission_id AND sub.status = 'D')
-                                  INNER JOIN judge_submissiontestcase tc
-                              ON sub.id = tc.submission_id
-                         GROUP BY cp.id, tc.batch, sub.id
-                     ) q
-                         INNER JOIN (
-                    SELECT prob, batch, MAX(r.batch_points) as max_batch_points
-                    FROM (
-                             SELECT cp.id          as `prob`,
-                                    tc.batch       as `batch`,
-                                    MIN(tc.points) as `batch_points`
-                             FROM judge_contestproblem cp
-                                      INNER JOIN
-                                  judge_contestsubmission cs
-                                  ON (cs.problem_id = cp.id AND cs.participation_id = %s)
-                                      LEFT OUTER JOIN
-                                  judge_submission sub
-                                  ON (sub.id = cs.submission_id AND sub.status = 'D')
-                                      INNER JOIN judge_submissiontestcase tc
-                                  ON sub.id = tc.submission_id
-                             GROUP BY cp.id, tc.batch, sub.id
-                         ) r
-                    GROUP BY prob, batch
-                ) p
-                ON p.prob = q.prob AND (p.batch = q.batch OR p.batch is NULL AND q.batch is NULL)
-                WHERE p.max_batch_points = q.batch_points
-                GROUP BY q.prob, q.batch
-            """, (participation.id, participation.id))
-
-            for problem_id, time, subtask_points in cursor.fetchall():
-                problem_id = str(problem_id)
-                time = from_database_time(time)
-                if self.config['cumtime']:
-                    dt = (time - participation.start).total_seconds()
-                else:
-                    dt = 0
-
-                if format_data.get(problem_id) is None:
-                    format_data[problem_id] = {'points': 0, 'time': 0}
-                format_data[problem_id]['points'] += subtask_points
-                format_data[problem_id]['time'] = max(dt, format_data[problem_id]['time'])
-
-            for problem_data in format_data.values():
-                penalty = problem_data['time']
-                points = problem_data['points']
-                if self.config['cumtime'] and points:
-                    cumtime += penalty
+        problem_subtask_data = participation.get_best_subtask_point()
 
         from judge.models.contest import ContestProblem
 
-        for problem_id, valueset in format_data.items():
-            queryset = ContestProblem.objects.filter(id=problem_id, contest=self.contest)
-            valueset['points'] *= queryset.first().points_scaling_factor
-            score += valueset['points']
+        for problem_id, subtask_data in problem_subtask_data.items():
+            cp = ContestProblem.objects.filter(id=problem_id, contest=self.contest).first()
+            scale = cp.points_scaling_factor
+            for index, batch in enumerate(subtask_data):
+                subtask_data[batch] *= scale
+            points = sum(subtask_data.values())
+            format_data[problem_id] = {
+                'points': points,
+                'time': 0,
+            }
+            score += points
 
         participation.cumtime = max(cumtime, 0)
         participation.score = round(score, self.contest.points_precision)
