f1db251e2099f67864f18df591e2a827ab6071b0
Phan Bình Nguyên Lâm <76941117+LLaammTTeerr@users.noreply.github.com>
2024-07-13 10:47:29 +0700

    no message

--- judge/views/about.py
+++ judge/views/about.py
@@ -1,5 +1,11 @@
+from django.conf import settings
 from django.views.generic import TemplateView
 
 
 class AboutView(TemplateView):
     template_name = 'about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['TEAM_MEMBERS'] = settings.TEAM_MEMBERS
+        return context
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
       }
     },
   },
-  plugins: [require('daisyui'),],
+  plugins: [require('@tailwindcss/typography'), require('daisyui'),],
   daisyui: {
     themes: ["light", "dark"],
   },
