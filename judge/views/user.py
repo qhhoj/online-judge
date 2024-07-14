c8551a8b668ae91286695cbb568390d2739126da
Phan Bình Nguyên Lâm <76941117+LLaammTTeerr@users.noreply.github.com>
2024-07-14 17:57:32 +0700

    localization and about page

--- judge/views/user.py
+++ judge/views/user.py
@@ -154,7 +154,7 @@ class UserPage(TitleMixin, UserMixin, DetailView):
 
 class CustomLoginView(LoginView):
     template_name = 'registration/login.html'
-    extra_context = {}
+    extra_context = {'title': 'Login'}
     authentication_form = CustomAuthenticationForm
     redirect_authenticated_user = True
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     extend: {
       fontFamily: {
         'sans': ['Lato', 'Roboto', 'Helvetica', 'Arial', 'sans-serif']
-      }
+      },
+      backgroundImage: {
+        'light-blue-pink-gradient': 'radial-gradient(circle, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%)',
+        'dark-blue-pink-gradient': 'radial-gradient(circle, rgba(63,94,251,1) 0%, rgba(252,70,107,1) 100%);',
+      },
     },
   },
   plugins: [require('@tailwindcss/typography'), require('daisyui'),],
