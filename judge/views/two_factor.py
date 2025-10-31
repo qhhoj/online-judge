74183fa5185e745228d697edba141de746f7b3a9
Phan Cong Dung <phancddev@gmail.com>
2025-11-01 00:13:01 +0700

    fix: replace deprecated SuccessURLAllowedHostsMixin with RedirectURLMixin for Django 4.1+ compatibility

--- judge/views/two_factor.py
+++ judge/views/two_factor.py
@@ -8,7 +8,7 @@ import qrcode
 import webauthn
 from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.views import SuccessURLAllowedHostsMixin
+from django.contrib.auth.views import RedirectURLMixin
 from django.http import Http404, HttpResponse, HttpResponseBadRequest, HttpResponseRedirect, JsonResponse
 from django.urls import reverse
 from django.utils.http import url_has_allowed_host_and_scheme as is_safe_url
@@ -226,7 +226,7 @@ class WebAuthnDeleteView(SingleObjectMixin, WebAuthnView):
         return HttpResponse()
 
 
-class TwoFactorLoginView(SuccessURLAllowedHostsMixin, TOTPView, ContextMixin):
+class TwoFactorLoginView(RedirectURLMixin, TOTPView, ContextMixin):
     form_class = TwoFactorLoginForm
     title = gettext_lazy('Perform Two-factor Authentication')
     template_name = 'registration/two_factor_auth.html'
