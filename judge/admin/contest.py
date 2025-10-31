10b0fe7823d086708c5805a9701ec1d82d1ce66f
Phan Cong Dung <phancddev@gmail.com>
2025-10-31 23:59:30 +0700

    fix: inherit ContestAdmin from SortableAdminBase for django-admin-sortable2 compatibility

--- judge/admin/contest.py
+++ judge/admin/contest.py
@@ -1,4 +1,4 @@
-from adminsortable2.admin import SortableInlineAdminMixin
+from adminsortable2.admin import SortableAdminBase, SortableInlineAdminMixin
 from django.contrib import admin
 from django.core.exceptions import PermissionDenied
 from django.db import connection, transaction
@@ -141,7 +141,7 @@ class ContestForm(ModelForm):
         }
 
 
-class ContestAdmin(NoBatchDeleteMixin, VersionAdmin):
+class ContestAdmin(SortableAdminBase, NoBatchDeleteMixin, VersionAdmin):
     fieldsets = (
         (None, {'fields': ('key', 'name', 'authors', 'curators', 'testers')}),
         (_('Settings'), {'fields': ('is_visible', 'use_clarifications', 'push_announcements', 'disallow_virtual',
